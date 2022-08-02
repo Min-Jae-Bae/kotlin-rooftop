e373c2806dc3f658deaafe427423e2578943b6c1
hanjuuuuuu <hanjucomss@naver.com>
2022-08-03 01:17:39 +0900

    data

--- app/src/main/java/com/example/kbsc_cooperate/data/RegionsRooftopDataSource.kt
+++ app/src/main/java/com/example/kbsc_cooperate/data/RegionsRooftopDataSource.kt
@@ -10,6 +10,11 @@ class RegionsRooftopDataSource @Inject constructor() {
             rooftop = MALIN,
             description = "갬성 루프탑",
             imageUrl = "https://images.unsplash.com/photo-1613066697301-d7dccfc86bb5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"
+        ),
+        ExploreModel(
+            rooftop = Flower,
+            description = "갬성 루프탑",
+            imageUrl = "https://images.unsplash.com/photo-1613066697301-d7dccfc86bb5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"
         )
     )
 }
--- app/src/main/java/com/example/kbsc_cooperate/data/Rooftops.kt
+++ app/src/main/java/com/example/kbsc_cooperate/data/Rooftops.kt
@@ -4,5 +4,11 @@ package com.example.kbsc_cooperate.data
 val MALIN = Rooftop(
     name = "Malin",
     region = "홍대",
-    price = "$20"
+    price = "20000"
+)
+
+val Flower = Rooftop(
+    name = "Flower",
+    region = "홍대",
+    price = "20000"
 )
