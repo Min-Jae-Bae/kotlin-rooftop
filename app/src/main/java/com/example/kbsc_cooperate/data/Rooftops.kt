45ff17857891f0109f1cec49679fd750f4ed2bca
hanjuuuuuu <hanjucomss@naver.com>
2022-08-04 01:51:00 +0900

    data추가, 로그인

--- app/src/main/java/com/example/kbsc_cooperate/data/Rooftops.kt
+++ app/src/main/java/com/example/kbsc_cooperate/data/Rooftops.kt
@@ -3,12 +3,36 @@ package com.example.kbsc_cooperate.data
 
 val MALIN = Rooftop(
     name = "Malin",
-    region = "홍대",
+    region = "홍대입구",
     price = "20000"
 )
 
-val Flower = Rooftop(
+val FLOWER = Rooftop(
     name = "Flower",
-    region = "홍대",
+    region = "홍대입구",
     price = "20000"
+)
+
+val BAR = Rooftop(
+    name = "Bar",
+    region = "연남동",
+    price = "20000"
+)
+
+val REST = Rooftop(
+    name = "Rest",
+    region = "강남",
+    price = "20000"
+)
+
+val FLEA = Rooftop(
+    name = "Flea",
+    region = "강남",
+    price = "20000"
+)
+
+val POOL = Rooftop(
+    name = "Pool",
+    region = "합정",
+    price = "40000"
 )
---
+++ app/src/main/java/com/example/kbsc_cooperate/login/LoginHome.kt
@@ -0,0 +1,246 @@
+package com.example.kbsc_cooperate.login
+
+import android.util.Patterns
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.kbsc_cooperate.R
+import com.example.kbsc_cooperate.ui.theme.KBSC_CooperateTheme
+
+@Composable
+fun LoginHome() {
+    val focusManager = LocalFocusManager.current
+
+    var email by remember {
+        mutableStateOf("")
+    }
+
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    /*유효한지 확인*/
+    val isEmailValid by derivedStateOf {
+        Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    val isPasswordValid by derivedStateOf {
+        password.length > 7
+    }
+
+    var isPasswordVisible by remember {
+        mutableStateOf(false)
+    }
+
+    Column(
+        modifier = Modifier
+            .background(color = Color.White)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_myprofile),
+            contentDescription = "Account Logo",
+            modifier = Modifier.size(150.dp)
+
+        )
+
+
+        Text(
+            text = "로그인 /  회원가입",
+            fontFamily = FontFamily.SansSerif,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Normal,
+            fontSize = 30.sp,
+            modifier = Modifier.padding(bottom = 16.dp)
+
+        )
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier.padding(all = 10.dp)
+            ) {
+                OutlinedTextField(
+                    value = email,
+                    onValueChange = { email = it },
+                    label = { Text("Email") },
+                    placeholder = { Text("abc@domain.com") },
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Email,
+                        imeAction = ImeAction.Next
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onNext = { focusManager.moveFocus(FocusDirection.Down) }
+                    ),
+                    isError = !isEmailValid,
+                    trailingIcon = {
+                        if (email.isNotBlank()) {
+                            IconButton(onClick = { email = ""}) {
+                                Icon(
+                                    imageVector = Icons.Filled.Clear,
+                                    contentDescription = "이메일 지우기")
+
+
+                            }
+                        }
+                    }
+                )
+
+
+                OutlinedTextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    label = { Text("Password") },
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Password,
+                        imeAction = ImeAction.Done
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onNext = { focusManager.moveFocus(FocusDirection.Down) }
+                    ),
+                    isError = !isPasswordValid,
+                    trailingIcon = {
+                        IconButton(
+                            onClick = { isPasswordVisible = !isPasswordVisible }
+                        ) {
+                            Icon(
+                                imageVector = if (isPasswordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff,
+                                contentDescription = "비밀번호 보이게 전환")
+                        }
+                    },
+                    visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation()
+                )
+
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Blue),
+                    enabled = isEmailValid && isPasswordValid
+                ) {
+                    Text(
+                        text = "로그인",
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black,
+                        fontSize = 16.sp
+                    )
+                }
+            }
+
+        Row(
+            horizontalArrangement = Arrangement.End,
+            modifier = Modifier.fillMaxWidth()
+        ) { TextButton(
+            onClick = { /*TODO*/ }
+        ) {
+            Text(
+                color = Color.Black,
+                fontStyle = FontStyle.Italic,
+                text = "비밀번호 찾기",
+                modifier = Modifier.padding(end = 10.dp)
+            )
+        }
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            enabled = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 16.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.White)
+        ) {
+            Text(
+                text = "계정 만들기",
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                fontSize = 16.sp
+            )
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            enabled = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 16.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.White)
+        ) {
+            Text(
+                text = "구글 계정으로 로그인",
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                fontSize = 16.sp
+            )
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            enabled = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 16.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.White)
+        ) {
+            Text(
+                text = "네이버로 로그인",
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                fontSize = 16.sp
+            )
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            enabled = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 16.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.White)
+        ) {
+            Text(
+                text = "카카오 계정으로 로그인",
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                fontSize = 16.sp
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    KBSC_CooperateTheme {
+        LoginHome()
+    }
+}
+
