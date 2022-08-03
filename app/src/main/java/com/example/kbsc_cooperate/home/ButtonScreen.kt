1243a7340082f793bf2a192233c470554b65f8ad
username <jujulee527@naver.com>
2022-08-04 04:51:44 +0900

    함수 합치기 + Row 클릭 펼치기 / 밑에 Row 밀려나기

--- app/src/main/java/com/example/kbsc_cooperate/home/ButtonScreen.kt
+++ app/src/main/java/com/example/kbsc_cooperate/home/ButtonScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.kbsc_cooperate.ui.theme.KBSC_CooperateTheme
 
@@ -31,194 +30,206 @@ class ButtonScreen : ComponentActivity() {
         }
     }
 }
+
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun Place(  // 위치
+fun ButtonScreenActivity(
 ) {
-    var isExpanded by remember { mutableStateOf(false) }
-    val extraPadding by animateDpAsState(
-        if (isExpanded) 300.dp else 0.dp,
-        animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy, stiffness = Spring.StiffnessLow
-        )
-    )
-    Surface(
-        color = if ( isExpanded) Color.White else MaterialTheme.colors.onSecondary,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 100.dp, horizontal = 50.dp),
-        border = BorderStroke(1.dp,color = if (isExpanded) MaterialTheme.colors.onSecondary else Color.White),
-        shape = RoundedCornerShape(15.dp),
-    ) {
-        Row(
-            modifier = Modifier
-                .clickable { isExpanded = !isExpanded }
-                .padding(15.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceAround
+    Column(modifier = Modifier
+        .padding(top = 100.dp)) {
+            Surface(
+                color =  MaterialTheme.colors.onSecondary,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 10.dp, horizontal = 50.dp),
+                border = BorderStroke(
+                    1.dp, color = MaterialTheme.colors.onSecondary
+                ),
+                shape = RoundedCornerShape(15.dp),
             ) {
-                Spacer(Modifier.width(98.dp))
-                Icon(
-                    Icons.Filled.Place, contentDescription = "place",
-                    modifier = Modifier.size(ButtonDefaults.IconSize),
-                    tint = Color.White
+                var isExpanded by remember { mutableStateOf(false) }
+                val extraPadding by animateDpAsState(
+                    if (isExpanded) 300.dp else 0.dp,
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
                 )
-                Spacer(modifier = Modifier.width(10.dp))
-                Text(
-                    if (isExpanded) "" else "위치", fontWeight = FontWeight.Bold,
-                    color = MaterialTheme.colors.background
-                )
-                Column(
+                Row( //위치
                     modifier = Modifier
-                        .weight(1f)
-                        .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+                        .clickable { isExpanded = !isExpanded }
+                        .padding(15.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceAround
                 ) {
+                    Spacer(Modifier.width(98.dp))
+                    Icon(
+                        Icons.Filled.Place, contentDescription = "place",
+                        modifier = Modifier.size(ButtonDefaults.IconSize),
+                        tint = if (isExpanded) MaterialTheme.colors.onSecondary else Color.White
+                    )
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Text(
+                        if (isExpanded) "" else "위치", fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colors.background
+                    )
+                    Column(
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+                    ) {
+                    }
+                }
+            }
+            Column() {
+                    Surface(
+                        color =  MaterialTheme.colors.onSecondary,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 10.dp, horizontal = 50.dp),
+                        border = BorderStroke(
+                            1.dp,
+                            color = MaterialTheme.colors.onSecondary
+                        ),
+                        shape = RoundedCornerShape(15.dp),
+                    ) {
+                        var isExpanded by remember { mutableStateOf(false) }
+                        val extraPadding by animateDpAsState(
+                            if (isExpanded) 300.dp else 0.dp,
+                            animationSpec = spring(
+                                dampingRatio = Spring.DampingRatioMediumBouncy,
+                                stiffness = Spring.StiffnessLow
+                            )
+                        )
+                        Row( //인원 수
+                            modifier = Modifier
+                                .clickable { isExpanded = !isExpanded }
+                                .padding(15.dp),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceAround
+                        ) {
+                            Spacer(Modifier.width(95.dp))
+                            Icon(
+                                Icons.Filled.Person, contentDescription = "person",
+                                modifier = Modifier.size(ButtonDefaults.IconSize),
+                                tint = if (isExpanded) MaterialTheme.colors.onSecondary else Color.White
+                            )
+                            Spacer(modifier = Modifier.width(10.dp))
+                            Text(
+                                if (isExpanded) "" else "인원 수", fontWeight = FontWeight.Bold,
+                                color = MaterialTheme.colors.background
+                            )
+                            Column(
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+                            ) {
+                            }
+                        }
+                    }
+                Column() {
+                        Surface(
+                            color = MaterialTheme.colors.onSecondary,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 10.dp, horizontal = 50.dp),
+                            border = BorderStroke(
+                                1.dp,
+                                color = MaterialTheme.colors.onSecondary
+                            ),
+                            shape = RoundedCornerShape(15.dp),
+                        ) {
+                            var isExpanded by remember { mutableStateOf(false) }
+                            val extraPadding by animateDpAsState(
+                                if (isExpanded) 300.dp else 0.dp,
+                                animationSpec = spring(
+                                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                                    stiffness = Spring.StiffnessLow
+                                )
+                            )
+                            Row( //날짜
+                                modifier = Modifier
+                                    .clickable { isExpanded = !isExpanded }
+                                    .padding(15.dp),
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.SpaceAround
+                            ) {
+                                Spacer(Modifier.width(98.dp))
+                                Icon(
+                                    Icons.Filled.CalendarToday, contentDescription = "date",
+                                    modifier = Modifier.size(ButtonDefaults.IconSize),
+                                    tint = if (isExpanded) MaterialTheme.colors.onSecondary else Color.White
+                                )
+                                Spacer(modifier = Modifier.width(10.dp))
+                                Text(
+                                    if (isExpanded) "" else "날짜", fontWeight = FontWeight.Bold,
+                                    color = MaterialTheme.colors.background
+                                )
+                                Column(
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+                                ) {
+                                }
+                            }
+                        }
+                    Column() {
+                            Surface(
+                                color = MaterialTheme.colors.onSecondary,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(vertical = 10.dp, horizontal = 50.dp),
+                                border = BorderStroke(
+                                    1.dp,
+                                    color = MaterialTheme.colors.onSecondary
+                                ),
+                                shape = RoundedCornerShape(15.dp),
+                            ) {
+                                var isExpanded by remember { mutableStateOf(false) }
+                                val extraPadding by animateDpAsState(
+                                    if (isExpanded) 300.dp else 0.dp,
+                                    animationSpec = spring(
+                                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                                        stiffness = Spring.StiffnessLow
+                                    )
+                                )
+                                Row( //테마
+                                    modifier = Modifier
+                                        .clickable { isExpanded = !isExpanded }
+                                        .padding(15.dp),
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    horizontalArrangement = Arrangement.SpaceAround
+                                ) {
+                                    Spacer(Modifier.width(98.dp))
+                                    Icon(
+                                        Icons.Filled.Check, contentDescription = "theme",
+                                        modifier = Modifier.size(ButtonDefaults.IconSize),
+                                        tint = if (isExpanded) MaterialTheme.colors.onSecondary else Color.White
+                                    )
+                                    Spacer(modifier = Modifier.width(10.dp))
+                                    Text(
+                                        if (isExpanded) "" else "테마", fontWeight = FontWeight.Bold,
+                                        color = MaterialTheme.colors.background
+                                    )
+                                    Column(
+                                        modifier = Modifier
+                                            .weight(1f)
+                                            .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+                                    ) {
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
-@OptIn(ExperimentalMaterialApi::class)
-@Composable
-fun Person( // 인원 수
-) {
-    var isExpanded by remember { mutableStateOf(false) }
-    val extraPadding by animateDpAsState(
-        if (isExpanded) 300.dp else 0.dp,
-        animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy, stiffness = Spring.StiffnessLow
-        )
-    )
-    Surface(
-        color = if ( isExpanded) Color.White else MaterialTheme.colors.onSecondary,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 170.dp, horizontal = 50.dp),
-        border = BorderStroke(1.dp,color = if (isExpanded) MaterialTheme.colors.onSecondary else Color.White),
-        shape = RoundedCornerShape(15.dp),
-    ) {
-        Row(
-            modifier = Modifier
-                .clickable { isExpanded = !isExpanded }
-                .padding(15.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceAround
-        ) {
-            Spacer(Modifier.width(95.dp))
-            Icon(
-                Icons.Filled.Person, contentDescription = "person",
-                modifier = Modifier.size(ButtonDefaults.IconSize),
-                tint = Color.White
-            )
-            Spacer(modifier = Modifier.width(10.dp))
-            Text(
-                if (isExpanded) "" else "인원 수", fontWeight = FontWeight.Bold,
-                color = MaterialTheme.colors.background
-            )
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ) {
-            }
-        }
-    }
-}
-@OptIn(ExperimentalMaterialApi::class)
-@Composable
-fun Date( // 날짜
-) {
-    var isExpanded by remember { mutableStateOf(false) }
-    val extraPadding by animateDpAsState(
-        if (isExpanded) 300.dp else 0.dp,
-        animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy, stiffness = Spring.StiffnessLow
-        )
-    )
-    Surface(
-        color = if ( isExpanded) Color.White else MaterialTheme.colors.onSecondary,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 240.dp, horizontal = 50.dp),
-        border = BorderStroke(1.dp,color = if (isExpanded) MaterialTheme.colors.onSecondary else Color.White),
-        shape = RoundedCornerShape(15.dp),
-    ) {
-        Row(
-            modifier = Modifier
-                .clickable { isExpanded = !isExpanded }
-                .padding(15.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceAround
-        ) {
-            Spacer(Modifier.width(98.dp))
-            Icon(
-                Icons.Filled.CalendarToday, contentDescription = "date",
-                modifier = Modifier.size(ButtonDefaults.IconSize),
-                tint = Color.White
-            )
-            Spacer(modifier = Modifier.width(10.dp))
-            Text(
-                if (isExpanded) "" else "날짜", fontWeight = FontWeight.Bold,
-                color = MaterialTheme.colors.background
-            )
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ) {
-            }
-        }
-    }
-}
-@OptIn(ExperimentalMaterialApi::class)
-@Composable
-fun Theme( //테마
-) {
-    var isExpanded by remember { mutableStateOf(false) }
-    val extraPadding by animateDpAsState(
-        if (isExpanded) 300.dp else 0.dp,
-        animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy, stiffness = Spring.StiffnessLow
-        )
-    )
-    Surface(
-        color = if ( isExpanded) Color.White else MaterialTheme.colors.onSecondary,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 310.dp, horizontal = 50.dp),
-        border = BorderStroke(1.dp,color = if (isExpanded) MaterialTheme.colors.onSecondary else Color.White),
-        shape = RoundedCornerShape(15.dp),
-    ) {
-        Row(
-            modifier = Modifier
-                .clickable { isExpanded = !isExpanded }
-                .padding(15.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceAround
-        ) {
-            Spacer(Modifier.width(98.dp))
-            Icon(
-                Icons.Filled.Check, contentDescription = "theme",
-                modifier = Modifier.size(ButtonDefaults.IconSize),
-                tint = Color.White
-            )
-            Spacer(modifier = Modifier.width(10.dp))
-            Text(
-                if (isExpanded) "" else "테마", fontWeight = FontWeight.Bold,
-                color = MaterialTheme.colors.background
-            )
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ) {
-            }
-        }
-    }
-}
+
 @Preview(showBackground = true)
 @Composable
 fun ButtonPreview(){
     KBSC_CooperateTheme {
-        Place()
-        Person()
-        Date()
-        Theme()
+        ButtonScreenActivity()
     }
 }
 
