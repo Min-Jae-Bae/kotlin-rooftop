f81d07000245d7d2baa5a7bffa63d51da1cb5fdc
username <jujulee527@naver.com>
2022-08-05 07:41:13 +0900

    8월 4일 - 인원 수  - 성인/어린이/유아/반려 동물 버튼 추가 (기능 x)

--- app/src/main/java/com/example/kbsc_cooperate/base/BaseUserInput.kt
+++ app/src/main/java/com/example/kbsc_cooperate/base/BaseUserInput.kt
@@ -3,13 +3,17 @@ package com.example.kbsc_cooperate.base
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -121,6 +125,25 @@ private fun RoofTopBaseUseInput(
             }
         }
     }
+}
+
+val IconButtonSizeModifier = Modifier.size(30.dp)
+@Composable
+fun RoundIconButtons(
+    modifier: Modifier = Modifier,
+    imageVector: ImageVector,
+    onClick: () -> Unit,
+    tint: Color = MaterialTheme.colors.onSecondary,
+    backgroundColor: Color = MaterialTheme.colors.background,
+    elevation: Dp = 4.dp, ){
+    Card(modifier = modifier.padding(all=4.dp).clickable{
+        onClick.invoke()}.then(IconButtonSizeModifier),
+        shape = CircleShape,
+        backgroundColor = backgroundColor,
+        elevation = elevation){
+            Icon(imageVector, contentDescription = "Plus or Minus icon", tint = tint)
+    }
+
 }
 
 @Preview
