271b91fd25bc5ad70598321cd005b3b54d1f5a11
Min-Jae-Bae <qoalswo123@icloud.com>
2022-08-18 02:56:13 +0900

    NavHost 큰 뼈대 제작

---
+++ app/src/main/java/com/example/kbsc_cooperate/navigation/content/ScreenContent.kt
@@ -0,0 +1,31 @@
+package com.example.kbsc_cooperate.navigation.content
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+
+@Composable
+fun ScreenContent(
+    name: String,
+    onClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            modifier = Modifier.clickable { onClick() },
+            text = name,
+            color = Color.Black,
+            fontSize = MaterialTheme.typography.h3.fontSize,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
