ce80246b0337cf0e8205b5c56bf3ae8bd7c1deda
Min-Jae-Bae <qoalswo123@icloud.com>
2022-08-26 16:29:36 +0900

    twix/hyun 풀리퀘 적용

--- app/src/main/java/com/example/kbsc_cooperate/home/MainActivity.kt
+++ app/src/main/java/com/example/kbsc_cooperate/home/MainActivity.kt
@@ -3,22 +3,9 @@ package com.example.kbsc_cooperate.home
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.VisibleForTesting
-import androidx.compose.animation.core.*
-import androidx.compose.animation.core.Spring.StiffnessLow
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
-import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.example.kbsc_cooperate.navigation.graph.RootNavigationGraph
@@ -36,38 +23,6 @@ class MainActivity : ComponentActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             KBSC_CooperateTheme {
-
-
-                /*    val widthSizeClass = calculateWindowSizeClass(this).widthSizeClass
-
-
-                    val navController = rememberNavController()
-                    NavHost(navController = navController, startDestination = Routes.Home.route) {
-                        composable(Routes.Home.route) {
-                            val mainViewModel = hiltViewModel<MainViewModel>()
-                            MainScreen(
-                                widthSize = widthSizeClass,
-                                onExploreItemClicked = {
-                                    launchDetailsActivity(context = this@MainActivity, item = it)
-                                },
-                                mainViewModel = mainViewModel
-                            )
-                        }
-                        composable(Routes.Calendar.route) {
-                            val parentEntry = remember {
-                                navController.getBackStackEntry(Routes.Home.route)
-                            }
-                            val parentViewModel = hiltViewModel<MainViewModel>(
-                                parentEntry
-                            )
-                            CalendarScreen(onBackPressed = {
-                                navController.popBackStack()
-                            }, mainViewModel = parentViewModel)
-
-
-
-                        }
-                    }*/
                 RootNavigationGraph(navController = rememberNavController())
             }
 
@@ -76,73 +31,7 @@ class MainActivity : ComponentActivity() {
 }
 
 
-
-@VisibleForTesting
-@Composable
-fun MainScreen(
-    widthSize: WindowWidthSizeClass,
-    onExploreItemClicked: OnExploreItemClicked,
-    mainViewModel: MainViewModel,
-) {
-    Surface(
-        modifier = Modifier.windowInsetsPadding(
-            WindowInsets.navigationBars.only(WindowInsetsSides.Start + WindowInsetsSides.End)
-        ),
-        color = MaterialTheme.colors.primary
-    ) {
-        val transitionState = remember { MutableTransitionState(mainViewModel.shownSplash.value) }
-        val transition = updateTransition(transitionState, label = "스플래쉬 반환")
-        val splashAlpha by transition.animateFloat(
-            transitionSpec = { tween(durationMillis = 100) }, label = "스플래쉬 알파"
-        ) {
-            if (it == SplashState.Shown) 1f else 0f
-        }
-        val contentAlpha by transition.animateFloat(
-            transitionSpec = { tween(durationMillis = 300) }, label = "내용 알파"
-        ) {
-            if (it == SplashState.Shown) 0f else 1f
-        }
-        val contentTopPadding by transition.animateDp(
-            transitionSpec = { spring(stiffness = StiffnessLow) }, label = "상단 내용 간격"
-        ) {
-            if (it == SplashState.Shown) 100.dp else 0.dp
-        }
-
-        Box {
-            MainContent(
-                modifier = Modifier.alpha(contentAlpha),
-                topPadding = contentTopPadding,
-                widthSize = widthSize,
-                onExploreItemClicked = onExploreItemClicked,
-                viewModel = mainViewModel
-            )
-        }
-    }
-}
-
-
-@Composable
-private fun MainContent(
-    modifier: Modifier = Modifier,
-    topPadding: Dp = 0.dp,
-    widthSize: WindowWidthSizeClass,
-    onExploreItemClicked: OnExploreItemClicked,
-    viewModel: MainViewModel,
-) {
-    Column(modifier = modifier) {
-        Spacer(Modifier.padding(top = topPadding))
-/*        RooftopHome(
-            widthSize = widthSize,
-            modifier = modifier,
-            onExploreItemClicked = onExploreItemClicked,
-            viewModel = viewModel
-        )*/
-    }
-}
-
-
 enum class SplashState { Shown, Completed }
-
 
 
 
